70ebcbeec07806ae1352b3eafc4dfc09a8d0ff3a
milonbhuiyan560257 <milonbhuiyan560257@gmail.com>
2026-08-20 15:05:11 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,21 +24,23 @@ android {
         kotlinCompilerExtensionVersion = "1.5.8"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
 }
 
 dependencies {
-    // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
 
-    // Coil
     implementation("io.coil-kt:coil-compose:2.6.0")
 
-    // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
