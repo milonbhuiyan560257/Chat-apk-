7d858abcc8584063ac0962e2e860d51da9e44578
milonbhuiyan560257 <milonbhuiyan560257@gmail.com>
2026-08-20 14:16:05 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ buildscript {
         google()
         mavenCentral()
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
@@ -13,4 +14,7 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    // KSP
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
