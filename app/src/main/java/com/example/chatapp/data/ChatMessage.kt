d789fec9f7245275772da851047b8abf602ee00f
milonbhuiyan560257 <milonbhuiyan560257@gmail.com>
2026-08-20 11:50:05 +0600

    Create ChatMessage.kt

---
+++ app/src/main/java/com/example/chatapp/data/ChatMessage.kt
@@ -0,0 +1,12 @@
+package com.example.chatapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "chat_messages")
+data class ChatMessage(
+    @PrimaryKey(autoGenerate = true) 
+    val id: Int = 0,
+    val messageText: String,
+    val timestamp: Long = System.currentTimeMillis()
+)
